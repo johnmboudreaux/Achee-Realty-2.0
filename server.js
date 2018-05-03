f1f5ccb3625512dbe8a8731d7c791ab8245c8475
johnmboudreaux <jhnbdrx@gmail.com>
2018-05-02 21:41:33 -0600

    working on getting search values into state to eventully call to an endpoint

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
 const express = require('express');
-const cors = require('express-cors');
-const bodyParser = require('body-parser');
-
-const app = express();
-const environment = process.env.NODE_ENV || 'development';
+// const cors = require('express-cors');
+const Zillow = require('node-zillow');
 const path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// const bodyParser = require('body-parser');
+const zillow = new Zillow('X1-ZWz1gbl96or2ff_4ok8b');
+const zwsid = process.env.ZWSID;
+const parameters = {
+  address: '6825 Garrison St',
+  citystatezip: 'arvada, co 80004',
+};
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors());
 app.set('port', process.env.PORT || 4000);
 app.locals.title = 'Achee Realty';
-app.use(express.static('www'));
+app.use(express.static('src'));
 
-const requireHTTPS = (request, response, next) => {
-  if (request.header('x-forwarded-proto') !== 'https') {
-    return response.redirect(`https://${request.header('host')}${request.url}`);
-  }
-  next();
-};
-if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
+// const requireHTTPS = (request, response, next) => {
+//   if (request.header('x-forwarded-proto') !== 'https') {
+//     return response.redirect(`https://${request.header('host')}${request.url}`);
+//   }
+//   next();
+// };
+// if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
 
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, '/public/index.html'));
 });
+//
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'www/index.html'));
+// });
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'www/index.html'));
-});
+// zillow.get('GetDeepSearchResults', parameters)
+//   .then((results) => {
+//     console.log(results);
+//     return results;
+//   });
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import key from '../../keys/apiKey';
 import './Hero.css';
 import Search from '../Search/Search';
-import key from '../../keys/apiKey';
 
 class Hero extends Component {
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      addressInputValue: '',
+      radius: '',
+      maxPriceInputValue: '',
+      minBeds: '',
+      minBaths: '',
+    };
+    this.updateState = this.updateState.bind(this);
     this.getListing = this.getListing.bind(this);
   }
+
   getListing() {
+    console.log('who dis');
     return fetch(
       `http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=${key}&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`,
     )
       .then(response => response.json())
       .then(parsedResponse => console.log(parsedResponse));
+  }
+
+  updateState(event) {
+    this.setState({
+      addressInputValue: event.target.value,
+      radius: event.target.value,
+      maxPriceInputValue: event.target.value,
+      minBeds: event.target.value,
+      minBaths: event.target.value,
+    });
   }
 
   render() {
@@ -26,7 +45,15 @@ class Hero extends Component {
             <section className="hero-image">
               <img src="assets/images/new-me1.png" alt="" />
             </section>
-            <Search />
+            <Search
+              addressInputValue={this.state.addressInputValue}
+              radius={this.state.radius}
+              maxPriceInputValue={this.state.maxPriceInputValue}
+              minBeds={this.state.minBeds}
+              minBaths={this.state.minBaths}
+              updateState={this.updateState}
+              getListing={this.getListing}
+            />
             <section id="hero-chevron" className="demo">
               <a href="#scrolling-images">
                 <span />
