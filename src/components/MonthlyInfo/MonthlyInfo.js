5f0d42d007c9a7eb64ef5ce7eefb9d400313f5b0
johnmboudreaux <jhnbdrx@gmail.com>
2018-07-15 15:18:39 -0600

    blog section is in and styled

--- src/components/MonthlyInfo/MonthlyInfo.js
+++ src/components/MonthlyInfo/MonthlyInfo.js
@@ -7,20 +7,26 @@ const MonthlyInfo = () => {
   return (
     <div>
       <div className="blog-title" >
-        <header>Cheryl&apos;s Tips</header>
+        <header>Maddies&apos;s Monthly Word Of Advice</header>
       </div>
       <div className="blog-contents">
         <div className="blog">
-          ChickensChickens Chickens Chickens Chickens Chickens Chickens Chickens
           Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
-          ChickensChickens Chickens Chickens ChickensChickens Chickens Chickens ChickensChickens
-          Chickens Chickens Chickens Chickens Chickens Chickens ChickensChickensChickensChickens
-          ChickensChickens Chickens Chickens Chickens Chickens ChickensChickens Chickens Chickens
-          Chickens Chickens Chickens Chickens Chickens ChickensChickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
+          Chickens Chickens Chickens Chickens Chickens Chickens Chickens Chickens
         </div>
-        <div className="blog-image">
-          image goes here
-        </div>
+        <div className="blog-image" />
       </div>
     </div>
   );
