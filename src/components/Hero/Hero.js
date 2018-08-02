a0bd6549cfd1607bcc9162ce5053a64283b164e1
johnmboudreaux <jhnbdrx@gmail.com>
2018-08-02 12:20:07 -0600

    scrolling working on full screen

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Scrollchor from 'react-scrollchor';
 import logoa from '../images/achee-logo (1).svg';
 import Navbar from '../Navbar/Navbar';
 import './Hero.css';
 
-const Hero = () => {
-  return (
-    <div className=" hero">
-      <div className="darken-overlay">
-        <Navbar />
-        <section className="hero-title-a">
-          <div>
-            <h2>
-              <span>
-                <img src={logoa} alt="fancy a" />
-              </span>CHEE REALTY SERVICES
-            </h2>
-            <h3>“Service Beyond The Sale”</h3>
-          </div>
-        </section>
-        <section id="hero-chevron" className="demo">
-          <Scrollchor
-            to="blog-title"
-            onClick={this.scrolling}
-            href="#scrolling-images"
+class Hero extends Component {
+  onKeyPressHandler() {
+    console.log('keypressed');
+  }
+
+  scrollToBottom() {
+    console.log('scrollToBottom');
+    document.querySelector('#footer').scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
+  render() {
+    return (
+      <div className=" hero">
+        <div className="darken-overlay">
+          <Navbar />
+          <section className="hero-title-a">
+            <div>
+              <h2>
+                <span>
+                  <img src={logoa} alt="fancy a" />
+                </span>CHEE REALTY SERVICES
+              </h2>
+              <h3>“Service Beyond The Sale”</h3>
+            </div>
+          </section>
+          <section
+            onKeyPress={this.onKeyPressHandler}
+            role="button"
+            tabIndex="0"
+            onClick={this.scrollToBottom}
+            id="hero-chevron"
+            className="demo"
           >
-            <span />
-            <span />
-            <span />
-          </Scrollchor>
-        </section>
+            <a>
+              <span />
+              <span />
+              <span />
+            </a>
+          </section>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Hero;
