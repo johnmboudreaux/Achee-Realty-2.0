1e8e204c5602ed06fbe05900bc4948037c2da874
johnmboudreaux <jhnbdrx@gmail.com>
2018-09-04 19:45:14 -0600

    added text in about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,7 +9,12 @@ const AboutMe = () => {
       <div className="container-fluid">
         <Row>
           <Col sm={8}>
-            <div className="text-body" />
+            <div className="text-body">
+              <p>Hi, I&apos;m Cheryl</p>
+              <p>I am the Real Estate Agent you have been looking for. Not only do I have a lifetime of experience with the Greater Baton Rouge and Acension parish areas, I have 15 years of experience selling and buying houses in and around them.</p>
+              <p>I am ready to help guide you through all of the sure to be overwhelming choices and impossible to to decide sacrifices that will need to made over the course of buying or selling a home.</p>
+              <p>I am a licensed agent as well as a broker, licensed and board certified by the state of Louisiana. With a proven track record like mine and the experience to overcome any obsticle put in front of me, I know I will be able to exceed all of your expectations.</p>
+            </div>
           </Col>
           <Col sm={4}>
             <div className="image" />
